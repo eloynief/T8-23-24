f9e9516aca0e699a0267d9bdebf61e571d8f378d
enieves <enieves@106-02.info2>
2024-05-07 14:21:25 +0200

    comentar

--- src/part1/ej1/Utils.java
+++ src/part1/ej1/Utils.java
@@ -1,24 +1,37 @@
 package part1.ej1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Utils {
-	
-	public double readDouble(Scanner sc) {
-		double num=0;
-		
-		
-		
-		
-		return num;
-	}
-	
-	public double readDouble(Scanner sc) {
-		double num=0;
-		
-		
-		
-		
-		return num;
-	}
+
+    public static double readDouble(Scanner sc) {
+        double valor = 0;
+
+        try {
+            System.out.println("Introduce un valor:");
+            valor = sc.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("El valor no es de tipo double.");
+        } finally {
+            sc.nextLine(); // Limpiar el buffer del Scanner
+        }
+
+        return valor;
+    }
+
+    public static int readInt(Scanner sc) {
+        int valor = 0;
+
+        try {
+            System.out.println("Introduce un valor:");
+            valor = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("El valor no es de tipo int.");
+        } finally {
+            sc.nextLine(); // Limpiar el buffer del Scanner
+        }
+
+        return valor;
+    }
 }
