29f2a3bf8209e300775e746105d5e81d821c1076
enieves <enieves@106-02.info2>
2024-05-07 14:17:29 +0200

    test1

---
+++ src/part1/ej1/Utils.java
@@ -0,0 +1,24 @@
+package part1.ej1;
+
+import java.util.Scanner;
+
+public class Utils {
+	
+	public double readDouble(Scanner sc) {
+		double num=0;
+		
+		
+		
+		
+		return num;
+	}
+	
+	public double readDouble(Scanner sc) {
+		double num=0;
+		
+		
+		
+		
+		return num;
+	}
+}
