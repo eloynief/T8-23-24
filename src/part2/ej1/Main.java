4e9e3d20257cf4da4714ee36923d6936b4c5fcf9
enieves <enieves@106-02.info2>
2024-05-09 13:40:52 +0200

    terinar

--- src/part2/ej1/Main.java
+++ src/part2/ej1/Main.java
@@ -1,10 +1,42 @@
 package part2.ej1;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//arraylist de ints
+		List<Integer> listaNumeros=new ArrayList<Integer>();
 
+		FileReader fr = null;
+		int num=0;
+		
+		int numLista=0;
+		
+		try {
+			fr= new FileReader("src\\part2\\NumerosReales");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		do {
+			
+			try {
+				num=fr.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println(num);
+			
+		}while(num!=-1);
+		
 	}
 
 }
